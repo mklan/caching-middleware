a17dbe9439e96fb4b5ef097b2a29d1fa6ad74d16
Matthias Klan <matthias.klan@gmail.com>
2019-02-14 01:34:21 +0100

    move entry directly to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-export const createCacheMiddleware = require('./middleware');
+const findAndUpdate = require('./utils');
+
+const cache = ({ primary, fallback, update, resultKey, context }) => async (req, res, next) => {
+    const ctx = await context(req);
+    const { result } = await findAndUpdate(ctx, { primary, fallback, update });
+    req[resultKey] = result;
+    next();
+}
+
+module.exports = cache;
